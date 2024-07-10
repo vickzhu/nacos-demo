f1b074c3753a3f366f3ae2ea14c0757340620d27
Administrator <Administrator@WINDOWS-9PBBFLV>
2024-07-11 02:18:32 +0800

    测试

--- service-provider/src/main/java/com/pentagon/nacos/service/provider/api/DemoController.java
+++ service-provider/src/main/java/com/pentagon/nacos/service/provider/api/DemoController.java
@@ -2,6 +2,7 @@ package com.pentagon.nacos.service.provider.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+	
+	@Value("${provider.name}")
+	private String name;
 
+	@Value("${provider.age}")
+	private int age;
+	
 	@RequestMapping(value = "/hello")
 	public String hello() {
+		System.out.println("name: " + name);
+		System.out.println("age:" + age);
 		logger.info("Hello from service provider-1");
 		return "Hello from service provider-1.";
 	}
